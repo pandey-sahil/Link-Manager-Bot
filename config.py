2ed991db62b1b7bd7ea3ac1e80cd29cefa3a1431
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2024-12-07 23:20:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,23 @@
-# Don't Edit
-
 import os
-
 from dotenv import load_dotenv
 load_dotenv()
 
-
-# Mandatory variables for the bot to start
 API_ID = int(os.getenv("API_ID", "Your Api Id"))
 API_HASH = os.environ.get("API_HASH", "Your Api Hash")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "").split(",")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
-ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
-#  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
-BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
+DATABASE_URL = os.getenv("DATABASE_URL", "Mongo URL")
+OWNER_ID = int(os.environ.get("OWNER_ID", "Owner Id"))
+
+if OWNER_ID not in ADMINS:
+    ADMINS.append(OWNER_ID)
+
+ADMINS.append(123456789)
+
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id"))
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel Username Without @")
+BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True")
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '')
 LINK_BYPASS = "False"
