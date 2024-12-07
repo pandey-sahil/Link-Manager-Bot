76abfbb63fbc6d01c3873c1c3128a112b34f8456
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2024-12-07 23:13:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/pandey-sahil/Link-Manager-Bot /ShortnerBot
+git clone https://github.com/pandey-sahil/Link-Manager-Bot.git /ShortnerBot
 cd /ShortnerBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
